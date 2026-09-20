cbb9ae475341b19ccbf0b235653b417b57806271
Viktoria <vika.shirokova2146@mail.ru>
2026-09-20 22:47:37 +0300

    created start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,9 +3,9 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
+from main import (tokenize, remove_stop_words, calculate_frequencies, get_top_n_words, create_language_profile, check_profile, compare_profiles_by_top_n, detect_language_by_top_n)
 
-
-def main() -> None:
+def main():
     """
     Launches an implementation.
     """
@@ -17,9 +17,33 @@ def main() -> None:
         stopwords = file.read().split("\n")
     with open("lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8") as file:
         en_text = file.read()
-    result = None
-    assert result, "Detection result is None"
 
+
+    print("Tokens: ", tokenize(de_text))
+    print("Tokens without stopwords: ", remove_stop_words(tokenize(de_text), stopwords))
+    frequencies = calculate_frequencies(remove_stop_words(tokenize(de_text), stopwords))
+    print("Frequency dictionary: ", frequencies)
+    top_words = get_top_n_words(frequencies, 7)
+    print("Top 7 popular words: ", top_words)
+
+    en_profile = create_language_profile('en', en_text, stopwords)
+    de_profile = create_language_profile('de', de_text, stopwords)
+    unknown_profile = create_language_profile('unknown', unknown_text, stopwords)
+    print('En_profile:', en_profile)
+    print('De_profile:', de_profile)
+    print('Unknown profile: ', unknown_profile)
+    print('En works: ', check_profile(en_profile))
+    print('De works: ', check_profile(de_profile))
+    print('Unknown profile works: ', check_profile(unknown_profile))
+
+    comparison_en = compare_profiles_by_top_n(unknown_profile, en_profile, 15)
+    comparison_de = compare_profiles_by_top_n(unknown_profile, de_profile, 15)
+    print('Comparison with en: ', comparison_en)
+    print('Comparison with de: ', comparison_de)
+
+    result = detect_language_by_top_n(unknown_profile, en_profile, de_profile, 15)
+    assert result, "Detection result is None"
+    print('Detected Language: ', result)
 
 if __name__ == "__main__":
     main()
